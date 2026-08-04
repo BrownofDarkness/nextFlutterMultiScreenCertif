1eebecdbce7eab65cc61af359e4abdf5513cddce
wilbrown_darkness <takoubrown@gmail.com>
2026-08-04 08:34:50 +0100

    init project

---
+++ android/app/src/main/kotlin/com/example/next_flutter_recipe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.next_flutter_recipe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/navigation/routes.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../main.dart';
+
+class AppRoutes {
+  static const String home = '/';
+  static const String login = 'login';
+  static const String recipes = 'recipes';
+  static const String details = 'details';
+  static const String adding = 'add-recipe';
+  static const String profile = 'profile';
+
+  AppRoutes._();
+
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final router = GoRouter(
+    initialLocation: "/$login",
+    debugLogDiagnostics: true,
+    navigatorKey: _rootNavigatorKey,
+
+    routes: [
+      GoRoute(
+        path: "/$login",
+        name: login,
+        builder: (context, state) => const MyHomePage(title: 'My recipe app'),
+      ),
+
+      GoRoute(
+        path: "/$recipes",
+        name: recipes,
+        builder: (context, state) {
+          return Center(child: Text("Listing all Recipes"));
+        },
+        routes: [
+          GoRoute(
+            path: '/:id',
+            name: details,
+            builder: (context, state) {
+              final id = state.pathParameters['id']!;
+              return Center(child: Text("Recipe details for ID: $id"));
+            },
+          ),
+
+          GoRoute(
+            path: "/$adding",
+            builder: (context, state) => Center(child: Text("Add Recipe Screen")),
+          ),
+        ]
+      ),
+
+      GoRoute(
+        path: "/$profile",
+        name: profile,
+        builder: (context, state) => Center(child: Text("Profile Screen")),
+      ),
+    ],
+  );
+}
